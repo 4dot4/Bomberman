9345e9010adeff1bbf1ca5f81e2b3cc22d541731
matt <controlgame@icloud.com>
2022-07-31 12:36:35 -0300

    added the map

--- src/main.zig
+++ src/main.zig
@@ -5,42 +5,62 @@ pub fn main() anyerror!void {
     // Initialization
     //--------------------------------------------------------------------------------------
     const screenWidth = 800;
-    const screenHeight = 450;
+    const screenHeight = 800;
 
     rl.InitWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
 
     rl.SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
-    const class = enum (u8) {grass,block,wall};
-    
+    //const types = enum (u8) {grass,rock,wall};
+    const block = struct { 
+        
+        
+        rect : rl.Rectangle = .{
+            .x = undefined,
+            .y = undefined,
+            .width = 80,
+            .height = 80,
+        },
+        color :rl.Color,
+        state : bool = true,
+
+    };
     const map:[10][10]u8 = [10][10]u8{
-        u8{2,2,2,2,2,2,2,2,2,2},
-        u8{2,0,0,0,0,0,0,0,0,2},
-        u8{2,0,0,0,0,0,0,0,0,2},
-        u8{2,0,0,0,0,0,0,0,0,2},
-        u8{2,0,0,0,0,0,0,0,0,2},
-        u8{2,0,0,0,0,0,0,0,0,2},
-        u8{2,0,0,0,0,0,0,0,0,2},
-        u8{2,0,0,0,0,0,0,0,0,2},
-        u8{2,0,0,0,0,0,0,0,0,2},
-        u8{2,2,2,2,2,2,2,2,2,2},
+        [10]u8{2,2,2,2,2,2,2,2,2,2},
+        [10]u8{2,0,0,0,1,1,0,0,0,2},
+        [10]u8{2,0,0,0,1,1,0,0,0,2},
+        [10]u8{2,0,0,0,1,1,0,0,0,2},
+        [10]u8{2,1,1,1,1,1,1,1,1,2},
+        [10]u8{2,1,1,1,1,1,1,1,1,2},
+        [10]u8{2,0,0,0,1,1,0,0,0,2},
+        [10]u8{2,0,0,0,1,1,0,0,0,2},
+        [10]u8{2,0,0,0,1,1,0,0,0,2},
+        [10]u8{2,2,2,2,2,2,2,2,2,2},
 
     };
 
+
     // Main game loop
     while (!rl.WindowShouldClose()) { // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
+    
+        var blocks = [3]block{
+            block{.color = rl.WHITE},
+            block{.color = rl.GRAY},
+            block{.color = rl.BLACK},
+        };
+        
         rl.BeginDrawing();
 
-        rl.ClearBackground(rl.WHITE);
+        rl.ClearBackground(rl.ORANGE);
 
-        rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LIGHTGRAY);
+        for (map) |valueY,indexY| {
+            for(valueY) |valueX,indexX| {
+                blocks[valueX].rect.x = @intToFloat(f32,indexX) * 80;
+                blocks[valueX].rect.y = @intToFloat(f32,indexY) * 80;
+                rl.DrawRectangleRec(blocks[valueX].rect,
+                                    blocks[valueX].color);
+            }
+        }
 
         rl.EndDrawing();
         //----------------------------------------------------------------------------------
